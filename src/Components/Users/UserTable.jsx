3cb1b9803eb773d12a304ee3f616aee5f5b21d51
naingaungzaw <naingaung9863@gmail.com>
2023-08-07 21:52:37 +0630

    adding table

--- src/Components/Users/UserTable.jsx
+++ src/Components/Users/UserTable.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
 
-const UserTable = () => {
-    return <div>UserTable</div>;
+const columns = [
+    {
+        field: "id",
+        headerName: "ID",
+        width: 140,
+        flex: 1,
+    },
+    {
+        field: "username",
+        headerName: "Username",
+        width: 140,
+        flex: 1,
+    },
+    {
+        field: "password",
+        headerName: "Password",
+        width: 140,
+        flex: 1,
+    },
+];
+
+export const UserTable = () => {
+    const rows = useSelector((state) => state.user.userList);
+
+    return (
+        <div style={{ height: 400, width: "100%" }}>
+            <DataGrid
+                columns={columns}
+                rows={rows}
+                initialState={{
+                    pagination: {
+                        paginationModel: {
+                            pageSize: 5,
+                        },
+                    },
+                }}
+                pageSizeOptions={5}
+                checkboxSelection
+            />
+        </div>
+    );
 };
 
 export default UserTable;
